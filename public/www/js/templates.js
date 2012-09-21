78f1d1a6b86d15b6cd0b410c58ce6862ded5e58e
= <=>
2012-09-21 13:35:28 -0400

    Added add and remove buttons to search results.

--- public/www/js/templates.js
+++ public/www/js/templates.js
@@ -12,6 +12,9 @@ templates = {
 	    "<a class='fill link' target='_blank' href='htt://notfound.com'>",
 	    "<span class='fill title'></span>",
 	    "<span class='fill description'></span>",
+	    "<section>",
+	    '<input class="btn" type="button" value="Add" />&nbsp;<input class="btn" type="button" value="Remove" />',
+	    "</section>",
 	    "</a>",
 	    "</li>"
 	],
