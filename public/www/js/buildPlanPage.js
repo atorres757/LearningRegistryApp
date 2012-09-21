e60224eff30412f59735524779f458abb59712a7
Vamsi Vidala <vamsi.vidala@forrent.com>
2012-09-21 12:25:33 -0400

    Added the search feature

--- public/www/js/buildPlanPage.js
+++ public/www/js/buildPlanPage.js
@@ -1,16 +1,41 @@
-var Search = {
+Search = {
+	resources: new Array(),
+	resourceIds: new Array(),
 	appendItem: function(resource){
 		var item = $(templates.resourceResult.join(""));
-		item.find(".link").href(resource.document);
+		item.find(".link").attr('href', resource.document);
 		item.find(".description").text(resource.description);
 		item.find(".title").text(resource.title);
 		
-		$(this.el).append(item);
+		$("#results").append(item);
 	},
 	renderResults: function(results){
 		results = $.parseJSON(results);
+		var curr = this.Search;
 		$.each(results, function(index, val){
-			console.log(val);
+			var valid = val._id;
+			curr.resources.push( val );
+			curr.resourceIds.push(val._id);
+			curr.appendItem(val);
+		});
+		curr.addMoreResultsButton();
+	},
+	addMoreResultsButton: function(){
+		$("#results").append($(templates.moreResultsButton.join("")));
+		$("#moreResults").bind('click', Search.loadMoreResults);
+	},
+	loadMoreResults: function() {
+		$("#moreResults").remove();
+		var qry = $('#searchQuery').val();
+		qry += "/count/10/toomit/"+Search.resourceIds.join(",");
+		store.query(qry, Search.renderResults)
+	},
+	searchResource: function(id) {
+		var curr = this.Search;
+		$.each(curr, function (index, val){
+			if(val._id){
+				return val;
+			}
 		});
 	}
 }
@@ -19,6 +44,7 @@ $(function () {
 	$('#searchQuery').bind('keypress', function(e){
 		if (e.keyCode == 13){
 			store.query($(this).val(), Search.renderResults)
+			return false;
 		}
 	});
 });
